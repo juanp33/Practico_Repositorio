fe197b535b2799badd00ffff29d2d4a016fcf313
Juan <33juanpintos33@gmail.com>
2024-06-09 11:22:40 -0300

    Agregar archivos de proyecto.

---
+++ pracRepositorio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pracRepositorio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
